a1f473c0154d82249930608d07c9de9b13271138
Diego Alonso Ojeda Medina <daom89@gmail.com>
2021-12-13 17:23:15 -0500

    Ejemplo Estructura

---
+++ partials/footer_imports.php
@@ -0,0 +1,5 @@
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="node_modules/feather-icons/dist/feather.min.js"></script>
+    <script src="node_modules/chart.js/dist/Chart.min.js"></script>
+    <script src="js/dashboard.js"></script>
